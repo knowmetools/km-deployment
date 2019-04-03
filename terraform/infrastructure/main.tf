cb297dc5bc7796385b33a80247df310b5b90005c
Chathan Driehuys <chathan@driehuys.com>
2019-04-03 00:23:37 -0400

    Remove unused AMI data source.

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -59,22 +59,6 @@ data "aws_acm_certificate" "webapp" {
 data "aws_caller_identity" "current" {
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 data "aws_iam_policy_document" "instance-assume-role-policy" {
   statement {
     actions = ["sts:AssumeRole"]
