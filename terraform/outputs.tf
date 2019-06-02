04e892a0d1d208bbeadef7c2685ee9be23fe8c26
Chathan Driehuys <chathan@driehuys.com>
2019-06-02 10:49:48 -0400

    Restore outputs for database credentials.
    These credentials are primarily used if we ever need to do manual
    database backups or restores.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,6 +7,24 @@ output "production_admin_password" {
   value     = module.deployment.production_admin_password
 }
 
+output "production_db_admin_password" {
+  sensitive = true
+  value     = module.prod_app.database_admin_password
+}
+
+output "production_db_admin_user" {
+  value = module.prod_app.database_admin_user
+}
+
+output "production_db_password" {
+  sensitive = true
+  value     = module.prod_app.database_password
+}
+
+output "production_db_user" {
+  value = module.prod_app.database_user
+}
+
 output "staging_admin_email" {
   value = var.admin_email
 }
@@ -14,4 +32,22 @@ output "staging_admin_email" {
 output "staging_admin_password" {
   sensitive = true
   value     = module.deployment.staging_admin_password
+}
+
+output "staging_db_admin_password" {
+  sensitive = true
+  value     = module.staging_app.database_admin_password
+}
+
+output "staging_db_admin_user" {
+  value = module.staging_app.database_admin_user
+}
+
+output "staging_db_password" {
+  sensitive = true
+  value     = module.staging_app.database_password
+}
+
+output "staging_db_user" {
+  value = module.staging_app.database_user
 }
